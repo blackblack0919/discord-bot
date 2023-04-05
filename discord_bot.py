811880547bebfa2ea993d857956c685a8bbc49b0
blackblack0919 <127098825+blackblack0919@users.noreply.github.com>
2023-04-05 14:00:07 +0800

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -4,7 +4,6 @@ import os
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from random import choice
-from core.classes import Cog_Extension
 
 intents= discord.Intents.all()
 
@@ -42,20 +41,75 @@ async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
 @bot.command()
-async def stock(ctx):
-    url = (f'https://tw.stock.yahoo.com/quote/2330')
-    r = requests.get(url) 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    await ctx.send(soup.fint("span", class_="Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)"))
+async def receipt(ctx):
+    url = 'https://invoice.etax.nat.gov.tw/index.html'
+    web = requests.get(url)    
+    web.encoding='utf-8'       
 
-class draw(Cog_Extension):
-    @commands.command()
-    async def draw(self,ctx):
-        member_list = []
-        guild = self.bot.get_guild(int(self.GUILDID_TOKEN))
-        for user in guild.members:            
-            if str(user.status) != "offline":
-                member_list.append(f"<@{user.id}>")
-        await ctx.message.reply(choice(member_list))
+    soup = BeautifulSoup(web.text, "html.parser")                   
+    td = soup.select('.container-fluid')[0].select('.etw-tbiggest')  
+    ns = td[0].getText() 
+    n1 = td[1].getText() 
+    n2 = [td[2].getText()[-8:], td[3].getText()[-8:], td[4].getText()[-8:]] 
+    await ctx.send(ns)
+    await ctx.send(n1)
+    await ctx.send(n2)
 
-bot.run("MTA4NjU0NDQ1NjIyMTc0MTExNg.GZ1-3M.o1leIDU-gsM9YwPbSZLftkyMlRVaQxevwayiyM")
+@bot.command()
+async def receipt_check(ctx, num):
+    url = 'https://invoice.etax.nat.gov.tw/index.html'
+    web = requests.get(url)    
+    web.encoding='utf-8'       
+
+    soup = BeautifulSoup(web.text, "html.parser")                   
+    td = soup.select('.container-fluid')[0].select('.etw-tbiggest')  
+    ns = td[0].getText() 
+    n1 = td[1].getText() 
+    n2 = [td[2].getText()[-8:], td[3].getText()[-8:], td[4].getText()[-8:]] 
+    
+    if num == ns: await ctx.send('恭喜你對中了1000萬元')
+    elif num == n1: await ctx.send('恭喜你對中200萬元')
+    for i in n2:
+        if num == i:
+            await ctx.send('恭喜你對中 20 萬元！')
+            break
+        elif num[-7:] == i[-7:]:
+            await ctx.send('恭喜你對中 4 萬元！')
+            break
+        elif num[-6:] == i[-6:]:
+            await ctx.send('恭喜你對中 1 萬元！')
+            break
+        elif num[-5:] == i[-5:]:
+            await ctx.send('恭喜你對中 4000 元！')
+            break
+        elif num[-4:] == i[-4:]:
+            await ctx.send('恭喜你對中 1000 元！')
+            break
+        elif num[-3:] == i[-3:]:
+            await ctx.send('恭喜你對中 200 元！')
+            break
+
+@bot.command()
+async def stock(ctx, num):
+    url = f'https://tw.stock.yahoo.com/quote/{num}' 
+    web = requests.get(url)                         
+    soup = BeautifulSoup(web.text, "html.parser")   
+    name = soup.find_all('h1')[1]             
+    money = soup.select('.Fz\(32px\)')[0]    
+    await ctx.send(f'{name.get_text()}: {money.get_text()}') 
+
+@bot.command()
+async def money(ctx):
+    url = 'https://rate.bot.com.tw/xrt'
+    web = requests.get(url)
+    web.encoding = 'utf-8'
+    soup = BeautifulSoup(web.text, "html.parser")
+    for i in range (19):
+        name = soup.select('hidden-phone print_show xrt-cur-indent')[i]
+        buyin = soup.select('rate-content-cash text-right print_hide')[i]
+        soldout = soup.select('rate-content-cash text-right print_hide')[i]
+        await ctx.send(f'{name.get_text()} {buyin.get_text()} {soldout.get_text()}')
+        
+bot.run("token")
+
+#發票的程式碼引用自:https://steam.oxxostudio.tw/category/python/spider/invoice-auto.html
